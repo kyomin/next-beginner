133e12993e8c4b1d239f8eafdedbd780ef235c65
jake.kyo <jake.kyo@kakaocorp.com>
2023-05-14 19:22:57 +0900

    레이아웃 짜기 & 여러 페이지 만들기(라우팅) 연습

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* Layout을 짜는 곳입니다. */
+import { Html, Head, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
+        {/* Navigation Bar */}
+        <div className='navbar'>
+          <Link href='/'>홈</Link>
+          <Link href='/list'>상품목록</Link>
+        </div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+/* 루트 페이지(/)입니다. */
 export default function Home() {
-  return <div>메인 페이지</div>;
+  return (
+    <div>
+      <h4 className='title'>Fresh Market</h4>
+      <p className='title-sub'>by Jake</p>
+    </div>
+  );
 }
