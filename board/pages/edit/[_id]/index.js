68893decc9fba6ffae0ee74483b583c722fffc82
jake.kyo <jake.kyo@kakaocorp.com>
2023-06-07 21:25:23 +0900

    게시판 프로젝트 - 로그인 여부 및 작성 여부에 따라 글 수정할 수 있도록 작업

--- board/pages/edit/[_id]/index.js
+++ board/pages/edit/[_id]/index.js
@@ -1,27 +1,56 @@
+import { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
 import { connectDB } from '@/util/database';
 import { ObjectId } from 'mongodb';
 
 export default function Edit({ post }) {
-  const { _id, title, description } = post;
+  const [user, setUser] = useState(null);
+  const { _id, title, description, author } = post;
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    const response = await axios.get('/api/auth/me');
+
+    setUser(response.data ? response.data.user : null);
+  };
 
   return (
     <div className='p-20'>
-      <h4>글 수정</h4>
+      {!user ? (
+        <Fragment>
+          <h4>글을 수정하려면 로그인해 주십시오!</h4>
+        </Fragment>
+      ) : user.email === author ? (
+        <Fragment>
+          <h4>글 수정</h4>
 
-      <form action='/api/post/update' method='POST'>
-        <input name='title' defaultValue={title} placeholder='글 제목 입력' />
-        <input
-          name='description'
-          defaultValue={description}
-          placeholder='글 내용 입력'
-        />
-        <input
-          style={{ display: 'none' }}
-          name='_id'
-          defaultValue={_id.toString()}
-        />
-        <button type='submit'>수정</button>
-      </form>
+          <form action='/api/post/update' method='POST'>
+            <input
+              name='title'
+              defaultValue={title}
+              placeholder='글 제목 입력'
+            />
+            <input
+              name='description'
+              defaultValue={description}
+              placeholder='글 내용 입력'
+            />
+            <input
+              style={{ display: 'none' }}
+              name='_id'
+              defaultValue={_id.toString()}
+            />
+            <button type='submit'>수정</button>
+          </form>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <h4>내가 작성한 글이 아니라 수정할 수 없습니다!</h4>
+        </Fragment>
+      )}
     </div>
   );
 }
